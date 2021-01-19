b0a9a9d014095d7c919aada070186872c0a4ab20
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2021-01-19 20:21:30 +0100

    nav_header_bottom

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,8 @@ let app = new Vue ({
             prefix : "(001)",
             number_phone1 : "88451234",
             number_phone2 : "88455438"
-        }
+        },
+        menu_nav : ["HOME","ABOUT","SERVICES","SHOWCASE","BLOG","CONTACT"]
     }
 
 });
