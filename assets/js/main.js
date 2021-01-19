654af4f90df7291a339e11d751b21ffe35416245
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2021-01-20 00:16:14 +0100

    section1 structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,25 @@ let app = new Vue ({
             number_phone1 : "88451234",
             number_phone2 : "88455438"
         },
-        menu_nav : ["HOME","ABOUT","SERVICES","SHOWCASE","BLOG","CONTACT"]
+        menu_nav : ["HOME","ABOUT","SERVICES","SHOWCASE","BLOG","CONTACT"],
+        counter: 0,
+        img_section1 : [
+            "assets/img/Group-36-2x.png",
+            "assets/img/Group-40-2x.png",
+            "assets/img/Group-35-2x.png"
+        ],
+        social_icon_section1 : ["FACEBOOK","YOUTUBE","INSTAGRAM","TWITTER"] 
+    },
+    methods: {
+        next(){
+            this.counter++;
+            if(this.counter > this.img_section1.length -1){
+                this.counter = 0;
+            }
+        }
+    },
+    created: function(){
+        setInterval(this.next, 5000);
     }
 
 });
