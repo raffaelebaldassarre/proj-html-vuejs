982f86d5d6e4c4c2e35785fc15657313c4376094
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2021-01-19 13:18:48 +0100

    nav_header_top

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let app = new Vue ({
+    el: "#app",
+    data : {
+        number_phone : {
+            prefix : "(001)",
+            number_phone1 : "88451234",
+            number_phone2 : "88455438"
+        }
+    }
+
+});
