c596cce11a3c0a4f2dd3636d46573c4876bc34b4
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2021-01-20 16:49:33 +0100

    section1 complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,30 +17,30 @@ let app = new Vue ({
             {
             text_section1_0 : 
                 {
-                h4 : "17 YEARS OF EXPERIENCE 00",
-                h1 : "We Are a <br> Wer Design <span class='agency_color'>Agency</span> 00",
-                p : "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,<br> there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the <br> Semantics, a large language ocean.Separated they live in Bookmarksgrove. 00"
+                h4 : "17 YEARS OF EXPERIENCE 01",
+                h1 : "We Are a <br> Web Design <span class='agency_color'>Agency</span> 01",
+                p : "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,<br> there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the <br> Semantics, a large language ocean.Separated they live in Bookmarksgrove. 01"
                 }
             },
             {
                 text_section1_1 : 
                 {
-                h4 : "17 YEARS OF EXPERIENCE 01",
-                h1 : "We Are a <br> Wer Design <span class='agency_color'>Agency</span> 01",
-                p : "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,<br> there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the <br> Semantics, a large language ocean.Separated they live in Bookmarksgrove. 01"
+                h4 : "17 YEARS OF EXPERIENCE 02",
+                h1 : "We Are a <br> Web Design <span class='agency_color'>Agency</span> 02",
+                p : "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,<br> there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the <br> Semantics, a large language ocean.Separated they live in Bookmarksgrove. 02"
                 }
             },
             {
                 text_section1_2 : 
                 {
-                h4 : "17 YEARS OF EXPERIENCE 02",
-                h1 : "We Are a <br> Wer Design <span class='agency_color'>Agency</span> 02",
-                p : "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,<br> there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the <br> Semantics, a large language ocean.Separated they live in Bookmarksgrove. 02"
+                h4 : "17 YEARS OF EXPERIENCE 03",
+                h1 : "We Are a <br> Web Design <span class='agency_color'>Agency</span> 03",
+                p : "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,<br> there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the <br> Semantics, a large language ocean.Separated they live in Bookmarksgrove. 03"
                 } 
             }
         ],
         
-        social_icon_section1 : ["FACEBOOK","YOUTUBE","INSTAGRAM","TWITTER"] 
+        social_icon_section1 : ["FACEBOOK","INSTAGRAM","YOUTUBE","TWITTER"] 
     },
     methods: {
         next(){
